281b7da121a8927cffb2a489f14a1e92abc31437
jguo13 <jguo13@gmail.com>
2020-05-18 17:26:11 -0400

    Update bc_app.cu

--- gunrock/app/bc/bc_app.cu
+++ gunrock/app/bc/bc_app.cu
@@ -181,10 +181,10 @@ cudaError_t RunTests(util::Parameters &parameters, GraphT &graph,
       }
   
   }
-
+ //edits done
+} 
   cpu_timer.Start();
   // Copy out results
-  GUARD_CU(problem.Extract(h_total_bc_values, h_sigmas, h_labels));
 
   for (VertexT j=0; j < end_src; j++)
   {
@@ -193,9 +193,8 @@ cudaError_t RunTests(util::Parameters &parameters, GraphT &graph,
      h_total_bc_values[j] += h_bc_values[j];   // aggregate the sums into the first array
   }
 
- //edits done
-}   
-//   GUARD_CU(problem.Extract(h_total_bc_values, h_sigmas, h_labels));
+  
+  GUARD_CU(problem.Extract(h_bc_values, h_sigmas, h_labels));
   util::PrintMsg("--------------------------\nH_total_BC_VALUES====0 " + std::to_string(h_total_bc_values[0]), !quiet_mode);
 
   
