a29ee85c499cbc0fc22399caf9cf0e6c55e34db7
jguo13 <jguo13@gmail.com>
2020-05-24 16:51:06 -0400

    Update bc_app.cu

--- gunrock/app/bc/bc_app.cu
+++ gunrock/app/bc/bc_app.cu
@@ -159,8 +159,6 @@ cudaError_t RunTests(util::Parameters &parameters, GraphT &graph,
       cpu_timer.Start();
       GUARD_CU(enactor.Enact(src));
       util::PrintMsg("___________enact done_______________" + std::to_string(src), !quiet_mode);
-      GUARD_CU(enactor.Release(target));
-//       GUARD_CU(problem.Release(target));
       //edits done
       } 
       util::PrintMsg("___________DONE_______________", !quiet_mode);
@@ -251,8 +249,8 @@ cudaError_t RunTests(util::Parameters &parameters, GraphT &graph,
 
 
   // Clean up
-//   GUARD_CU(enactor.Release(target));
-//   GUARD_CU(problem.Release(target));
+  GUARD_CU(enactor.Release(target));
+  GUARD_CU(problem.Release(target));
        //edited for loop end
 
   delete[] h_bc_values;
